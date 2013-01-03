b00988579f12dbd064924e12ffb88168bbd5ad88
ViViDboarder <ViViDboarder@gmail.com>
2013-01-03 12:20:25 -0500

    Improved Storing of String and Handling of #Anchors

--- Github-Forks-Menu_extension/forks.js
+++ Github-Forks-Menu_extension/forks.js
@@ -1,36 +1,36 @@
 // TODO: Find another way to get this header style from Github CSS
 var liHeaderStyle = 'font-weight: bold; color: #111; text-shadow: 1px 1px 0 white; padding: 8px 10px 5px 10px; background: #F6F8F8; background: -moz-linear-gradient(#F6F8F8, #E9EEEE); background: -webkit-linear-gradient(#F6F8F8, #E9EEEE); background: linear-gradient(#F6F8F8, #E9EEEE); border-bottom: 1px solid #F0F3F3; border-top-left-radius: 4px; border-top-right-radius: 4px;';
 
-var listItemHtml = '\
-        <li class="selector-item js-navigation-item js-navigation-target selected"> \
-            <a href="/{0}" class="forks-repo-button"> \
-                <label> \
-                  <input checked="checked" id="do_included" name="do" type="radio" value="included"> \
-                  <h4>{0}</h4> \
-                  <p>{1}</p> \
-                </label> \
-            </a> \
-          </li> \
-    ';
+var listItemHtml = [
+        '<li class="selector-item js-navigation-item js-navigation-target selected">',
+            '<a href="/{0}" class="forks-repo-button">',
+                '<label>',
+                    '<input checked="checked" id="do_included" name="do" type="radio" value="included">',
+                    '<h4>{0}</h4>',
+                    '<p>{1}</p>',
+                '</label>',
+            '</a>',
+        '</li>'
+    ].join('\n');
 
-var buttonHtml = ' \
-<div class="context-menu-container js-menu-container js-context-menu"> \
-    <span class="minibutton switcher bigger js-menu-target"> \
-      <span class="js-context-button"> \
-          <span class="mini-icon mini-icon-fork"></span>Forks \
-      </span> \
-    </span> \
-    <div class="context-pane js-menu-content"> \
-      <a href="javascript:;" class="close js-menu-close"><span class="mini-icon mini-icon-remove-close"></span></a> \
-      <div class="context-title">Existing forks</div> \
-      <div class="context-body pane-selector"> \
-        <ul class="js-navigation-container"> \
-        </ul> \
-      </div> \
-    </div> \
-  </div> \
-</div> \
-    ';
+var buttonHtml = [
+        '<div class="context-menu-container js-menu-container js-context-menu">',
+            '<span class="minibutton switcher bigger js-menu-target">',
+                '<span class="js-context-button">',
+                    '<span class="mini-icon mini-icon-fork"></span>forks',
+                '</span>',
+            '</span>',
+            '<div class="context-pane js-menu-content">',
+                '<a href="javascript:;" class="close js-menu-close"><span class="mini-icon mini-icon-remove-close"></span></a>',
+                '<div class="context-title">existing forks</div>',
+                '<div class="context-body pane-selector">',
+                    '<ul class="js-navigation-container">',
+                    '</ul>',
+                '</div>',
+            '</div>',
+        '</div>',
+        '</div>'
+    ].join('\n');
 
 String.format = function() {
   var s = arguments[0];
@@ -39,13 +39,15 @@ String.format = function() {
   }
 
   return s;
-}
+};
+
+var scheme = "api.github.com";
 
 // Returns the repo name from the url
 function getRepoName() {
     var repo_url = window.location.href.substr(window.location.href.indexOf(".com/") + 5);
     // Repo url might have more than just the repo name
-    var repoParts = repo_url.split("/");
+    var repoParts = repo_url.split(/[\/#]+/);
     var repo = repoParts[0] + '/' + repoParts[1];
 
     return repo;
@@ -56,7 +58,7 @@ function getRepoName() {
 function getRepoData() {
     var repo = getRepoName();
 
-    var repoUrl = "https://api.github.com/repos/" + repo;
+    var repoUrl = "https://" + scheme + "/repos/" + repo;
     $.getJSON(repoUrl, repoResults);
 }
 
@@ -115,7 +117,7 @@ function repoResults(data) {
 // Gets fork data from fork api
 function getForkData() {
     var repo = getRepoName();
-    var forksUrl = "https://api.github.com/repos/" + repo + "/forks?sort=watchers";
+    var forksUrl = "https://" + scheme + "/repos/" + repo + "/forks?sort=watchers";
     console.log(forksUrl);
     $.getJSON(forksUrl, forksResults);
 }
